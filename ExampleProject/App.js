984945375749a7f3878c10e050ec9286020d8f5b
Hadas Zeilberger <hadaszeilberger@Hadass-MacBook-Pro.local>
2018-10-26 18:15:59 -0400

    detox tests

--- ExampleProject/App.js
+++ ExampleProject/App.js
@@ -20,7 +20,7 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
+      <View testID="welcome" style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
