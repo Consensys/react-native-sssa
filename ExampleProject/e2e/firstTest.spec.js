675102589ee0ce53e7eb094cb981dc5fde272693
Hadas Zeilberger <hadaszeilberger@Hadass-MacBook-Pro.local>
2018-10-26 17:11:30 -0400

    travis

---
+++ ExampleProject/e2e/firstTest.spec.js
@@ -0,0 +1,14 @@
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+
+  it('should show hello screen after tap', async () => {
+    await expect(element(by.text('Welcome to React Native!'))).toBeVisible();
+  });
+
+})
