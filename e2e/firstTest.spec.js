45d99ed3800afd8c0c436ced6c6e02a89136ee86
Hadas Zeilberger <hadaszeilberger@Hadass-MacBook-Pro.local>
2018-11-07 21:02:56 -0500

    utility functions and combine function

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -10,4 +10,11 @@ describe('Example', () => {
   it('should have the correct length', async () => {
     await expect(element(by.id('randombits'))).toHaveText('14');
   });
+  it('will output an array of shares', async () => {
+    await expect(element(by.id('shares'))).toHaveText('7');
+  });
+   it('will output the correct length', async () => {
+    await expect(element(by.id('oneShare'))).toHaveText('true');
+  });
 });
+
