6cbdf65d695d27b1711e187b27c52c0500efb4d8
Hadas Zeilberger <hadaszeilberger@Hadass-MacBook-Pro.local>
2018-11-09 14:30:35 -0500

    small changes

--- ExampleGrinder/App.js
+++ ExampleGrinder/App.js
@@ -6,14 +6,8 @@
  */
 /*eslint no-unused-vars: "warn"*/
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { randomBitGenerator } from '../lib/randomBitGenerator.js';
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
 
 type Props = {};
 
